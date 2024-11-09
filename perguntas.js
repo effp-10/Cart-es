4175a7af3e9611dcd74a95efe9b17baec2b1204b
effp-10 <00001130035803SP@al.educacao.sp.gov.br>
2024-11-08 22:43:17 -0300

    Flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esporte',
+    'Qual país inventou o esporte?',
+    'Grécia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior rio do Brasil?',
+    'Rio Amazonas'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o homem pisou na lua?',
+    'Em 1969'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que a palavra justice significa em português?',
+    'Justiça'
+)
